9e42c1718a338ba317a631920e54be0e853216c1
shuubhambadiwal <shubhambadiwal2@gmail.com>
2025-05-21 23:54:38 +0530

    Update the index for dashboard

--- src/pages/Index.tsx
+++ src/pages/Index.tsx
@@ -1,9 +1,11 @@
+import { Dashboard } from "@/components/dashboard/Dashboard";
+
 export default function Index() {
     return (
       <div className="space-y-4">
         <h1 className="text-3xl font-bold tracking-tight">Dashboard</h1>
         <p className="text-muted-foreground">Overview of patient data and statistics.</p>
- 
+        <Dashboard/>
       </div>
     );
   }
